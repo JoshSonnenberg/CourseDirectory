e97cb2e5d17c6f14527339db0113d151d0d58c4e
Josh Sonnenberg <jsonnenberg@solstice-consulting.com>
2018-08-02 12:40:17 -0500

    MVC Testing
    * Added test for greeting endpoint on courses

--- applications/course-service/src/test/java/com/solstice/courseservice/CourseServiceTests.java
+++ applications/course-service/src/test/java/com/solstice/courseservice/CourseServiceTests.java
@@ -1,25 +1,50 @@
 package com.solstice.courseservice;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CourseServiceTests {
 
-//    @Test
-//    public void testGetAllCoursesWhenNoneExist() {
-//        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-//        CourseRepository courseRepository = new CourseRepository(jdbcTemplate);
-//        CourseController controller = new CourseController(courseRepository, new CoursePresenter());
-//        List<CourseInfo> expectedCourses = null;
-//        assertEquals(controller.courses(), expectedCourses);
-//    }
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testGreeting() throws Exception {
+        this.mockMvc.perform(get("/courses/greeting"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Course Service"));
+
+    }
+
+    @Test
+    public void testGetAllCoursesWhenNoneExist() throws Exception {
+        // Extract CourseService from CourseController
+        // Rename existing CourseService to CourseServiceApplication or CourseServiceRunner
+        // In CourseServiceTest:
+            // Mock CourseService dependencies
+            // Mock return value of courses
+            // Test against mock
+        // Here:
+            // Hit courses endpoint
+            // Check against expected value NOTE: was Justin mocking this?
+    }
 
 //    @Test
 //    public void testGetAllCoursesWhenOneExist() {
